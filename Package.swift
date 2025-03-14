c4998d89a71beb4c265d2c7ea9b4d6e0dbc2df8a
okferret <wanggang3@staff.sina.com.cn>
2025-03-14 18:55:31 +0800

    update => rename

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "libical-spm",
+    name: "libical",
     platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
